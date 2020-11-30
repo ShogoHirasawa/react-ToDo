ccb4b0c2a87f22f29388fc93c8a1efd563d1b7e6
Shogo Hirasawa <origami.no.1@gmail.com>
2020-12-01 01:35:54 +0900

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,30 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="ToDoを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p>未完了のToDo</p>
+
+        <li>あああああ</li>
+        <button>削除</button>
+        <button>完了</button>
+        <li>いいいいい</li>
+        <button>削除</button>
+        <button>完了</button>
+      </div>
+      <div className="complete-area">
+        <p>完了のToDo</p>
+
+        <li>あああああ</li>
+        <button>戻す</button>
+        <li>あああああ</li>
+        <button>戻す</button>
+      </div>
+    </>
+  );
 };
